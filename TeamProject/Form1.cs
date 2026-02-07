1214fe369a355cc791fbf0162ad64e0b2eea81fa
Александр Богданов <work.2005git@mail.ru>
2026-02-07 22:58:55 +0300

    добавление, удаление, сохранение состояний списка

--- TeamProject/Form1.cs
+++ TeamProject/Form1.cs
@@ -12,9 +12,33 @@ namespace TeamProject
 {
     public partial class Form1 : Form
     {
+        private List<string> _state;
+        
+
         public Form1()
         {
             InitializeComponent();
+            _state = new List<string>();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {}
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if (txtBox.Text.Replace(" ", "") != "") 
+            {
+         
+                listBox.Items.Add(txtBox.Text);
+                _state.Add(txtBox.Text);        
+                txtBox.Text = "";
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (listBox.Text != "") 
+                listBox.Items.RemoveAt(listBox.SelectedIndex);
         }
     }
 }
